91059a206425a83dd5fb244b197d4f672a2e3e1b
Jack Coutts <70660958+Jack-Coutts@users.noreply.github.com>
2022-02-07 14:51:56 +0000

    Delete Software Dev. Web App directory

--- Software Dev. Web App/main.py
+++
@@ -1,8 +0,0 @@
-# import the app factory (the app)
-from Website import create_app
-
-app = create_app()
-
-if __name__ == '__main__': # only if we run this file will we execute the line below
-    # run the Flask app & start a web server
-    app.run(debug=True) # debug means that if code changes server is auto re-run
