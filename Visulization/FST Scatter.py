a0f52f82acfdabe72b52cf79873f144f49aaf7e9
Shiloh Alleyne <83500298+ShilohAlleyne@users.noreply.github.com>
2022-02-27 15:47:32 +0000

    Add files via upload

--- Visulization/FST Scatter.py
+++ Visulization/FST Scatter.py
@@ -3,42 +3,47 @@ import pandas as pd
 
 pd.set_option("display.max_rows", None, "display.max_columns", None)
 
-input = {('bebG-cheG'): [0.03543337, 0.0315438, 0.00944336, 0.04456595,
-                         0.02946088, 0.02491669],
-         ('bebG-esnG'): [0.02142849, 0.05582269, 0.11426458, 0.32566496,
-                         0.13148831, 0.15959616],
-         ('bebG-gbrG'): [0.06790303, 0.00856417, 0.02699473, 0.00512476,
-                         0.00028977, 0.16291702],
-         ('bebG-pelG'): [0.01733013, 0.03025453, 0.02291235, 0.10282672,
-                         0.12648646, 0.07740511],
-         ('cheG-esnG'): [0.05810838, 0.06303388, 0.19060241, 0.29045066,
-                         0.15537565, 0.07898431],
-         ('cheG-gbrG'): [0.19165262, 0.07118688, 0.07546849, 0.06820209,
-                         0.04661426, 0.07510459],
-         ('cheG-pelG'): [0.00366681, 0.01947445, 0.07142241, 0.11278983,
-                         0.15059607, 0.01258583],
-         ('esnG-gbrG'): [0.09610099, 0.0440949, 0.03436135, 0.28341565,
-                         0.09397178, 0.01500246],
-         ('esnG-pelG'): [0.02068164, 0.04520733, 0.04398022, 0.25958801,
-                         0.24068202, 0.0321381],
-         ('gbrG-pelG'): [0.14944938, 0.04998906, 0.0107279, 0.07794422,
-                         0.13721246, 0.02307796]}
+input = {
+         ('bebG', 'cheG'): {1: 0.033965142017862875, 2: 0.020338952458864853, 3: 0.02829640374557564, 4: 0.011969472066051177},
+         ('bebG', 'esnG'): {1: 0.033950165902009506, 2: 0.22964449202370138, 3: 0.1439432092931027, 4: 0.104780270647522},
+         ('bebG', 'gbrG'): {1: 0.04513583112102886, 2: 0.019293514982147947, 3: 0.043925149596432, 4: 0.06649401774038718},
+         ('bebG', 'pelG'): {1: 0.022848016101287363, 2: 0.05748160747314586, 3: 0.1200093297802129, 4: 0.1675742115967348},
+         ('cheG', 'esnG'): {1: 0.06016570098124014, 2: 0.2416076185889834, 3: 0.14795362128785383, 4: 0.0929974027699578},
+         ('cheG', 'gbrG'): {1: 0.14513217793633923, 2: 0.07257541173870458, 3: 0.052244525622411726, 4: 0.029221633500032658},
+         ('cheG', 'pelG'): {1: 0.010662917429055646, 2: 0.08879712970015427, 3: 0.13107618191745593, 4: 0.12901670484988106},
+         ('esnG', 'gbrG'): {1: 0.0773255971059701, 2: 0.1920806796852942, 3: 0.09010871811293061, 4: 0.050164036520277365},
+         ('esnG', 'pelG'): {1: 0.030874994446445793, 2: 0.18016576094794665, 3: 0.22694910747247382, 4: 0.31932352488357674},
+         ('gbrG', 'pelG'): {1: 0.10799152555853336, 2: 0.04385200742540727, 3: 0.12841662227928322, 4: 0.2364799447642988}
+         }
 
-start = 200000
-stop = 200600
-step = 100
+start = 1000
+stop = 2000
+step = 250
+nstep = int((stop - start)/step)
 
 
 # NOTE: Converts 200000 to 2M for better legend formating
 def strink(num):
     if len(str(num)) <= 5:
-        snum = str(num/1000)+'k'
+        snum = str("{:.2f}".format(num/1000)+'k')
         return snum
     elif len(str(num)) >= 6:
-        snum = str(num/1000000)+'M'
+        snum = str("{:.2f}".format(num/1000000)+'M')
         return snum
     else:
         pass
+
+
+# NOTE: Replaces the step keys in the input dict with their boundaries
+def replace_keys(old_dict, key_dict):
+    new_dict = {}
+    for key in old_dict.keys():
+        new_key = key_dict.get(key, key)
+        if isinstance(old_dict[key], dict):
+            new_dict[new_key] = replace_keys(old_dict[key], key_dict)
+        else:
+            new_dict[new_key] = old_dict[key]
+    return new_dict
 
 
 # NOTE: Generates a scatter graph if given a dictionary of values
@@ -48,33 +53,39 @@ def FSTscatter(input, start, stop, step):
              (strink(n)+'-'+strink(min(n+step, stop)))
              for n in range(start, stop, step)
              ]
+    keydict = dict(zip(list(range(1, nstep+1, 1)), bounds))
     # NOTE: Makes a nested dictionary
-    nest = {}
-    for k, v in input.items():
-        # NOTE: Assigns the bounds to each value in the input dictionary
-        nest[k] = dict(zip(bounds, v))
-
+    nest = replace_keys(input, keydict)
+    print(nest)
     # NOTE: Creates df for graph
     df = pd.DataFrame.from_dict(nest, orient='index').stack().reset_index()
-    df.columns = ['Pop', 'Range', 'FST']
+    df['Pop'] = df[['level_0', 'level_1']].agg('-'.join, axis=1)
+    del df['level_0']
+    del df['level_1']
+    df.columns = ['Range', 'FST', 'Pop']
+    print(df)
 
     # NOTE: plots the scatter graph
     fig = px.scatter(df, x="Pop", y="FST", color="Range",
-                     color_discrete_sequence=px.colors.qualitative.Antique_r,
-                     labels={"Range": "FST Region on Chromosome (bp)",
+                     color_discrete_sequence=px.colors.qualitative.Dark24,
+                     labels={"Range": "FST Region on Chromosome (bp) ",
                              "Pop": "Population Group",
                              "FST": "FST Value"},
-                     title="Hudson FST")
+                     title="Hudson FST",
+                     animation_frame="Range",
+                     animation_group="Pop")
     fig.update_traces(marker=dict(size=12))
 
-    # NOTE: Sets the fonts
+    # NOTE: Sets the fonts and layout
     fig.update_layout(font_family="Times New Roman",
                       font_color="Black",
                       title_font_family="Times New Roman",
                       title_font_color="Black",
-                      legend={'traceorder': 'reversed'})
+                      legend={'traceorder': 'reversed'},
+                      showlegend=False,
+                      yaxis_range=[-0.1, 0.5])
 
-    fig.add_hline(y=0.12, line_width=3, line_dash="dash", line_color="gray")
+    fig.add_hline(y=0.12, line_width=2, line_dash="dash", line_color="gray")
 
     fig.show()
 
