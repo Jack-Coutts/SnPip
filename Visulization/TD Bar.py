0d2bc7d7d2813f8f69782b53f7e84dd3de036c14
Shiloh Alleyne <83500298+ShilohAlleyne@users.noreply.github.com>
2022-03-02 12:20:43 +0000

    Add files via upload

--- Visulization/TD Bar.py
+++ Visulization/TD Bar.py
@@ -2,25 +2,67 @@ import plotly.express as px
 import pandas as pd
 from input import input
 
+pd.set_option("display.max_rows", None, "display.max_columns", None)
+
+start = 41577800
+stop = 41589900
+
+
+# NOTE: Converts 200000 to 2M for better legend formating
+def strink(num):
+    if len(str(num)) <= 5:
+        snum = str("{:.6f}".format(num/1000)+'k')
+        return snum
+    elif len(str(num)) >= 6:
+        snum = str("{:.6f}".format(num/1000000)+'M')
+        return snum
+    else:
+        pass
+
 
 # NOTE: Plots a Barchart for Tajima's D
-def TD_Bar(input):
+def TD_Bar(input, start, stop):
+    # NOTE: Creates a list of nested keys from input dict
+    ik = []
     for v in input.values():
-        nstep = len(v)
+        for key in v.keys():
+            ik.append(key)
 
-    steplabels = list(range(1, nstep + 1, 1))
+    # NOTE: records the number of steps in the input data
+    vlen = []
+    for v in input.values():
+        vlen.append(len(v.values()))
+    print(vlen)
 
-    # NOTE: Creates a nested dictionary for the input
-    nest = {}
-    for k, v in input.items():
-        nest[k] = dict(zip(steplabels, v))
+    # NOTE: Calculates the step size of the input data
+    step = int((stop - start)/vlen[0])
+    print(step)
 
-    # NOTE: Creates a df for the graph
-    df = pd.DataFrame.from_dict(nest, orient='index').stack().reset_index()
+    # NOTE: Creates the range caterogies
+    bounds = [
+             (strink(n+1)+''+'-'+''+strink(min(n+step, stop)))
+             for n in range(start, stop, step)
+             ]
+    print(bounds)
+
+    # NOTE: Maps each nested key from the input dict to a boundary
+    first = ik[0:vlen[0]]
+    keydict = dict(zip(first, bounds))
+    print(len(keydict))
+
+    # NOTE: Creates df for graph
+    df = pd.DataFrame.from_dict(input, orient='index').stack().reset_index()
+    df = df.fillna('')
     df.columns = ['Pop', 'Step', 'TD']
+    df['Step'].replace(keydict, inplace=True)
+    print(df)
 
     # NOTE: Plots the graph
-    fig = px.bar(df, x='Step', y='TD', color='Pop', barmode='overlay',
+    fig = px.bar(df, y='TD', x='Step', color='Pop', barmode='overlay',
+                 labels={"Step": "Region on Chromosome (bp) ",
+                         "Pop": "Population Group",
+                         "TD": "Tajima's D"},
+                 title="Tajima's Diversity",
                  color_discrete_sequence=px.colors.qualitative.G10)
 
     # NOTE: Sets the fonts and layout
@@ -29,38 +71,9 @@ def TD_Bar(input):
                       title_font_family="Times New Roman",
                       title_font_color="Black")
 
-    # NOTE: Creates a range slider
-    fig.update_layout(
-        xaxis=dict(
-            rangeselector=dict(
-                buttons=list([
-                    dict(count=200000,
-                         label="200000bp",
-                         step="all",
-                         stepmode="backward"),
-                    dict(count=400000,
-                         label="400000bp",
-                         step="all",
-                         stepmode="backward"),
-                    dict(count=600000,
-                         label="600000bp",
-                         step="all",
-                         stepmode="backward"),
-                    dict(count=800000,
-                         label="800000bp",
-                         step="all",
-                         stepmode="backward"),
-                    dict(step="all")
-                ])
-            ),
-            rangeslider=dict(
-                visible=True
-            ),
-            type="linear"
-        )
-    )
+
 
     fig.show()
 
 
-TD_Bar(input)
+TD_Bar(input, 41577800, 41589900)
