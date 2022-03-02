77e9ad0b1c8d372b6a0180b706d224f3c4f7664e
Dhiraj Kormocha <93210870+dkormocha@users.noreply.github.com>
2022-03-02 18:15:12 +0000

    Add files via upload

--- integrated_search_app3/Website/static/js/finalDW.js
+++ integrated_search_app3/Website/static/js/finalDW.js
@@ -1,37 +1,37 @@
-$(document).ready(function(){
+$(document).ready(function(){           // function to download stat tables separately 
   $('#export').click(function() {
     var titles = [];
     var data = [];
 
  
-    $('#my_id th').each(function() {
+    $('#my_id th').each(function() {      // finds any item with id my_id and then the th attribute and pushes the content
       titles.push($(this).text());
     });
 
 
-    $('#my_id td').each(function() {
+    $('#my_id td').each(function() {    // finds any item with id my_id and then the td attribute and pushes the content
       data.push($(this).text());
     });
     
 
-    var CSVString = prepCSVRow(titles, titles.length, '');
+    var CSVString = prepCSVRow(titles, titles.length, '');    // csv string
     CSVString = prepCSVRow(data, titles.length, CSVString);
 
 
-    var downloadLink = document.createElement("a");
+    var downloadLink = document.createElement("a"); //creates a csv file dynamically 
     var blob = new Blob(["\ufeff", CSVString]);
     var url = URL.createObjectURL(blob);
     downloadLink.href = url;
-    downloadLink.download = "Hudson_FST.csv";
+    downloadLink.download = "Hudson_FST.tsv";
 
 
-    document.body.appendChild(downloadLink);
+    document.body.appendChild(downloadLink);  //download link created 
     downloadLink.click();
     document.body.removeChild(downloadLink);
   });
 
 
-  function prepCSVRow(arr, columnCount, initial) {
+  function prepCSVRow(arr, columnCount, initial) {    // takes the table data from html and formats it for csv download
     var row = ''; 
     var delimeter = ','; 
     var newLine = '\r\n'; 
@@ -65,7 +65,7 @@ $(document).ready(function(){
 
 
 
-$(document).ready(function(){
+$(document).ready(function(){             // same function but for different stat table
   $('#export1').click(function() {
     var titles = [];
     var data = [];
@@ -89,7 +89,7 @@ $(document).ready(function(){
     var blob = new Blob(["\ufeff", CSVString]);
     var url = URL.createObjectURL(blob);
     downloadLink.href = url;
-    downloadLink.download = "Shannon_Diversity.csv";
+    downloadLink.download = "Shannon_Diversity.tsv";
 
 
     document.body.appendChild(downloadLink);
@@ -157,7 +157,7 @@ $(document).ready(function(){
     var blob = new Blob(["\ufeff", CSVString]);
     var url = URL.createObjectURL(blob);
     downloadLink.href = url;
-    downloadLink.download = "Tajima_D.csv";
+    downloadLink.download = "Tajima_D.tsv";
 
 
     document.body.appendChild(downloadLink);
@@ -201,17 +201,17 @@ $(document).ready(function(){
 
 
 $(document).ready(function(){
-  $('#dwMod').click(function() {
+  $('#export3').click(function() {
     var titles = [];
     var data = [];
 
 
-    $('#pop th').each(function() {
+    $('#my_id3 th').each(function() {
       titles.push($(this).text());
     });
 
 
-    $('#pop td').each(function() {
+    $('#my_id3 td').each(function() {
       data.push($(this).text());
     });
 
@@ -223,7 +223,7 @@ $(document).ready(function(){
     var blob = new Blob(["\ufeff", CSVString]);
     var url = URL.createObjectURL(blob);
     downloadLink.href = url;
-    downloadLink.download = "SNP_Summary.csv";
+    downloadLink.download = "HaplotypeD.tsv";
 
 
     document.body.appendChild(downloadLink);
